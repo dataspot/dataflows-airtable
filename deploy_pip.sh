07e1ce8c5fa56cb9e1aaaacc3eb6551d1efc67ec
Adam Kariv <adam.kariv@gmail.com>
2023-06-18 17:46:15 +0300

    deploy-pip script

--- deploy_pip.sh
+++ deploy_pip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf dist/ || true
 python3 setup.py sdist bdist_wheel
-python -m twine upload dist/*
+python -m twine upload -c .pypirc --repository dataflows_airtable --verbose dist/*
